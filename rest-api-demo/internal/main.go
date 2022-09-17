fa47561bcafc8e000e55849b9e46ca9fc5838996
djamel-eddine <djamelmahrougui@gmail.com>
2022-09-17 18:29:57 +0100

    init server

---
+++ rest-api-demo/internal/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+) 
+
+var port = "5000"
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello world, %q \n", html.EscapeString(r.URL.Path))
+    })
+
+    log.Printf("Listening on port %s", port)
+
+    log.Fatal(http.ListenAndServe(":"+port, nil))
+}
